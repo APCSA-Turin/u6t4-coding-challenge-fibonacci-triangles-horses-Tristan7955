475effd797d5205b62c6cdecb57c3388fb69deac
Tristan7955 <tjordan7955@bths.edu>
2025-01-07 16:22:01 +0000

    Init commit

--- src/main/java/com/example/project/ArrayPrinter.java
+++ src/main/java/com/example/project/ArrayPrinter.java
@@ -1,4 +1,19 @@
 package com.example.project;
 
-
+public class ArrayPrinter {
+    public static String printableString(int[] numList) {
+      String str = "[";
+      
+      for (int i = 0; i < numList.length - 1; i++) {
+        str += numList[i];
+        str += ", ";
+      }
+      int lastIndex = numList.length - 1;
+      str += numList[lastIndex];
+      str += "]";
+      
+      return str;
+    }
+}
+  
   
--- src/main/java/com/example/project/Point.java
+++ src/main/java/com/example/project/Point.java
@@ -1,22 +1,34 @@
 package com.example.project;
 
 public class Point {
-    private int x;
-    private int y;
-    
-    public Point(int x, int y) {
+  private int x;
+  private int y;
+  
+  public Point(int x, int y) {
       this.x = x;
       this.y = y;
-    }
-    
-    // Returns distance between this Point object and another Point object
-    public double distanceTo(Point other) {
-      /* IMPLEMENT ME */
-    }
-  
-    // Returns a string in the format: (x, y)
-    public String pointInfo() {
-      /* IMPLEMENT ME */
-    }
+  }
+
+  public int getX() {
+      return x;
+  }
+
+  public int getY() {
+      return y;
   }
   
+  public void setX(int newX) {
+    x = newX;
+  }
+
+  public void setY(int newY) {
+    y = newY;
+  }
+  public double distanceTo(Point other) {
+      return Math.sqrt(Math.pow(this.x - other.getX(), 2) + Math.pow(this.y - other.getY(), 2));
+  }
+
+  public String pointInfo() {
+      return "(" + x + ", " + y + ")";
+  }
+}
